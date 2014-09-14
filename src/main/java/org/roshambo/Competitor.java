95d8892602d61384904e8c3236eae039dec6f104
mancmelou <mancmelou@me.com>
2014-09-13 23:34:28 -0700

    update docs

--- src/main/java/org/roshambo/Competitor.java
+++ src/main/java/org/roshambo/Competitor.java
@@ -25,12 +25,13 @@ package org.roshambo;
  */
 public interface Competitor {
     /**
-     * Values that the engage method can return
+     * Values that the engage method must return
      */
-    enum Throw {ROCK, PAPER, SCISSORS};
+    enum Throw { ROCK, PAPER, SCISSORS }
 
     /**
      * This method will be called when battling another competitor.
+     * It should always return on of the three throws defined inside the enum above.
      *
      * @return Weapon
      */
@@ -40,9 +41,9 @@ public interface Competitor {
      * This method will be called after your opponents throws.
      * This is the input for your AI.
      *
-     * @param round   Round number
-     * @param victory Set to true if you have won this round
-     * @param myThrow Your throw
+     * @param round         Round number
+     * @param victory       Set to true if you have won this round
+     * @param myThrow       Your throw
      * @param opponentThrow Opponent's throw
      */
     void feedback(int round, boolean victory, Throw myThrow, Throw opponentThrow);
