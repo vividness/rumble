e85225edd2e00d4a46f0b33b1cc2ad030acb5f22
mancmelou <mancmelou@me.com>
2014-09-13 23:05:12 -0700

    remove helper methods; add some basic setters and getters

--- src/main/java/org/roshambo/Match.java
+++ src/main/java/org/roshambo/Match.java
@@ -1,8 +1,6 @@
 package org.roshambo;
 
-
 import java.util.HashMap;
-import java.util.Map;
 
 public class Match {
     private int numRounds;
@@ -14,10 +12,6 @@ public class Match {
     private HashMap<Integer,HashMap<String, String>> results;
     private HashMap<String, Integer> totals = new HashMap<String, Integer>();
 
-    public Match(Competitor player1, Competitor player2) {
-        this(player1, player2, 10);
-    }
-
     public Match(Competitor player1, Competitor player2, int numRounds) {
         this.player1      = player1;
         this.player2      = player2;
@@ -28,6 +22,42 @@ public class Match {
         this.totals.put("Ties", 0);
         this.totals.put(player1.name(), 0);
         this.totals.put(player2.name(), 0);
+    }
+
+    public void fightUntilFinish() {
+        while (currentRound <= numRounds) {
+            fight(currentRound++);
+        }
+    }
+
+    public void fightNextRound() {
+        fight(currentRound++);
+    }
+
+    public Competitor getPlayer1() {
+        return player1;
+    }
+
+    public Competitor getPlayer2() {
+        return player2;
+    }
+
+    public HashMap<Integer,HashMap<String, String>> getResults() {
+        return results;
+    }
+
+    public HashMap<String, Integer> getTotals() {
+        return totals;
+    }
+
+    private void fight(int round) {
+        Competitor.Throw throwP1 = player1.engage();
+        Competitor.Throw throwP2 = player2.engage();
+
+        Competitor winner = decideWinner(throwP1, throwP2);
+
+        updateResults(round, winner, throwP1, throwP2);
+        notifyCompetitors(round, winner, throwP1, throwP2);
     }
 
     private Competitor decideWinner(Competitor.Throw throwP1, Competitor.Throw throwP2) {
@@ -53,58 +83,6 @@ public class Match {
         }
     }
 
-    public void fightUntilFinish() {
-        while (currentRound <= numRounds) {
-            fight(currentRound++);
-        }
-    }
-
-    public void fightNextRound() {
-        fight(currentRound++);
-    }
-
-    public HashMap<Integer,HashMap<String, String>> getResults() {
-        return results;
-    }
-
-    public void printResults() {
-        System.out.format("Round,%s,%s,Winner\n", player1.name(), player2.name());
-
-        for (Map.Entry<Integer, HashMap<String, String>> entry : results.entrySet()) {
-            Integer round   = entry.getKey();
-            String  weapon1 = entry.getValue().get(player1.name());
-            String  weapon2 = entry.getValue().get(player2.name());
-            String  winner  = entry.getValue().get("WINNER");
-
-            System.out.format("%d,%s,%s,%s\n", round, weapon1, weapon2, winner);
-        }
-
-        System.out.println();
-        System.out.format("%s: %s,%s: %s,Ties: %s\n", player1.name(), totals.get(player1.name()), player2.name(), totals.get(player2.name()), totals.get("Ties"));
-    }
-
-    public void printLastRoundResult() {
-        HashMap<String, String> result = results.get(currentRound);
-
-        String  weapon1 = result.get(player1.name());
-        String  weapon2 = result.get(player2.name());
-        String  winner  = result.get("WINNER");
-
-        System.out.format("Round,%s,%s,Winner\n", player1.name(), player2.name());
-        System.out.format("%d,%s,%s,%s\n", currentRound, weapon1, weapon2, winner);
-        System.out.format("%s: %s,%s: %s,Ties: %s\n", player1.name(), totals.get(player1.name()), player2.name(), totals.get(player2.name()), totals.get("Ties"));
-    }
-
-    private void fight(int round) {
-        Competitor.Throw throwP1 = player1.engage();
-        Competitor.Throw throwP2 = player2.engage();
-
-        Competitor winner = decideWinner(throwP1, throwP2);
-
-        updateResults(round, winner, throwP1, throwP2);
-        notifyCompetitors(round, winner, throwP1, throwP2);
-    }
-
     private void notifyCompetitors(int round, Competitor winner, Competitor.Throw throwP1, Competitor.Throw throwP2) {
         player1.feedback(round, winner == player1, throwP1, throwP2);
         player2.feedback(round, winner == player2, throwP2, throwP1);
@@ -118,7 +96,7 @@ public class Match {
 
         if (winner == null) {
             totals.put("Ties", totals.get("Ties") + 1);
-            result.put("WINNER", "Tie");
+            result.put("WINNER", "[Tie]");
         } else {
             result.put("WINNER", winner.name());
             totals.put(winner.name(), totals.get(winner.name()) + 1);
